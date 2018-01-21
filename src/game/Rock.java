b94799b66561dd28f575d157edeb3ea2d3a6d811
NickHill1999 <nkhlshnd99@gmail.com>
2018-01-21 12:45:34 -0500

    Added Rock Class

---
+++ src/game/Rock.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Nikhil
+ */
+
+public class Rock {
+    private int posX = 0;
+    private int posY = 0;
+    public static ImageIcon rockImage;
+    
+    public Rock(int x, int y){
+        
+    }
+}
